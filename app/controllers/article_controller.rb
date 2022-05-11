05fa1e1ef181835848ba8212c141082eb160fbd2
nivya <nivya.ratha@gmail.com>
2022-05-11 17:59:12 +0000

    List of articles

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,4 +2,8 @@ class ArticleController < ApplicationController
     def show
         @article =  Article.find(params[:id])
     end
+    
+    def index
+        @articles = Article.all
+    end
 end
